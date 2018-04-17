41f5556de3d16c0a7457a34de622fc30059deec2
Dima Selivonchyk <snuyp@yandex,ru>
2018-04-17 14:24:24 +0300

    Create project

---
+++ app/src/main/java/com/example/dima/flickrtesttask/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.dima.flickrtesttask.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Dima on 17.04.2018.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
